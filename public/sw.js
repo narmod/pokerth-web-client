92bb49c64d492467fb14fac8720979801eaa3471
narmod <arnaud.obscur@gmail.com>
2026-05-23 18:37:57 +0200

    feat: Service Worker minimal pour PWA offline

---
+++ public/sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'pokerth-v1';
+const ASSETS = ['/', '/pokerth-client.html', '/pokerth.js', '/pokerth.css'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
+  self.skipWaiting();
+});
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', function(e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
