4eec2e88724a83b2c44b110bfcce92e4919bf542
narmod <arnaud.obscur@gmail.com>
2026-06-01 17:59:40 +0200

    i18n: ajoute preActionTitle/preActionHint dans les 31 langues restantes (option A : termes de poker laisses en anglais) ; bump cache v67

--- public/modules/lang/sk.mjs
+++ public/modules/lang/sk.mjs
@@ -10,6 +10,7 @@ export const meta = {
 };
 
 export const strings = {
+    preActionTitle:'Náhľad akcií', preActionHint:'ťuknite na karty pre zatvorenie',
     blindsNextTip:'o {n} hier', blindsEveryMin:'každých {n} min',
     connect: 'Pripojiť',
     disconnect: '✕ Odpojiť',
