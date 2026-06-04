bcbf51df1fb095c658a404eb8c39f588213a9dad
narmod <arnaud.obscur@gmail.com>
2026-06-04 12:22:22 +0200

    chore: bump BUILD_VERSION to 0.2.155

--- public/pokerth.js
+++ public/pokerth.js
@@ -9039,4 +9039,4 @@ function renderPlayersList() {
   }).join('');
 }
 
-;(function(){ window.BUILD_VERSION='0.2.154'; try{ var b=document.getElementById('cf-build'); if(b) b.textContent='\u00b7 build '+window.BUILD_VERSION; }catch(e){} })();
+;(function(){ window.BUILD_VERSION='0.2.155'; try{ var b=document.getElementById('cf-build'); if(b) b.textContent='\u00b7 build '+window.BUILD_VERSION; }catch(e){} })();
